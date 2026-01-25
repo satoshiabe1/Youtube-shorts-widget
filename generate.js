f93a6ba123e0f33496d25d8477e088f3e3f3e873
Satoshi <sadavisca@gmail.com>
2026-01-26 00:56:41 +0900

    Update generate.js

--- generate.js
+++ generate.js
@@ -7,19 +7,21 @@ const CHANNEL_ID = "UC7WTGZV5NNPCLJH9wc8e0xQ";
 const url =
   `https://www.googleapis.com/youtube/v3/search?` +
   `part=snippet&channelId=${CHANNEL_ID}` +
-  `&order=date&type=video&videoDuration=short&maxResults=4&key=${API_KEY}`;
+  `&order=date&type=video&maxResults=4&key=${API_KEY}`;
 
 const res = await fetch(url);
 const json = await res.json();
 
-let html = `
-<div class="shorts-wrapper">
-`;
+let html = `<div class="shorts-wrapper">\n`;
 
 json.items.forEach(v => {
   html += `
   <div class="shorts-item">
-    <iframe src="https://www.youtube.com/embed/${v.id.videoId}" allowfullscreen></iframe>
+    <iframe
+      src="https://www.youtube.com/embed/${v.id.videoId}"
+      frameborder="0"
+      allowfullscreen>
+    </iframe>
   </div>`;
 });
 
