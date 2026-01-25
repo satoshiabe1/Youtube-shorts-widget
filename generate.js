f3f5fd6fded97ae5f93dc6c4ac9016e9ba726d5a
Satoshi <sadavisca@gmail.com>
2026-01-26 00:31:08 +0900

    Create generate.js

---
+++ generate.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import fs from "fs";
+
+const API_KEY = process.env.YOUTUBE_API_KEY;
+const CHANNEL_ID = "oishiakiko";
+
+const url =
+  `https://www.googleapis.com/youtube/v3/search?` +
+  `part=snippet&channelId=${CHANNEL_ID}` +
+  `&order=date&type=video&videoDuration=short&maxResults=4&key=${API_KEY}`;
+
+const res = await fetch(url);
+const json = await res.json();
+
+let html = `
+<div class="shorts-wrapper">
+`;
+
+json.items.forEach(v => {
+  html += `
+  <div class="shorts-item">
+    <iframe src="https://www.youtube.com/embed/${v.id.videoId}" allowfullscreen></iframe>
+  </div>`;
+});
+
+html += `</div>`;
+
+fs.writeFileSync("shorts.html", html);
